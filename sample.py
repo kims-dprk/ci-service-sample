6b3f094d9774d58081f975c5e9d9e6189d2c5665
kims-dprk <32179789+kims-dprk@users.noreply.github.com>
2017-09-22 07:30:51 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,19 +1,1 @@
 
-
-import sys
-import os
-def fib():
-    x = 0
-    a, b = 1, 1
-
-
-    while True:
-        yield    a
-        a, b = b, a + b
-
-
-if __name__ == '__main__':
-    for i, num in enumerate(fib()):
-        print(num)
-        if i >= 9:
-            break
